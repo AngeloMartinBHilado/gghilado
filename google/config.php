21cc6bef78ba4e0c3f9609c19e412bbbd1174b53
AngeloMartinBHilado <56312511+AngeloMartinBHilado@users.noreply.github.com>
2020-03-20 14:18:01 +0800

    Update config.php

--- google/config.php
+++ google/config.php
@@ -3,9 +3,9 @@ require_once 'vendor/autoload.php';
 
 //google ------ credits: john paul maja :D <3 
 $google_client = new Google_Client();
-$google_client->setClientId('947482041440-33e8qh9nlu3vemrq1brcp9jdbb1a582v.apps.googleusercontent.com');
-$google_client->setClientSecret('ACl_dvm1FbABsfeSpnR9QxvX');
-$google_client->setRedirectUri('https://localhost/etact-maja2-master/index.php');
+$google_client->setClientId('889421152360-r47q290i5v29ilfs62cdrfrcth51ac4t.apps.googleusercontent.com');
+$google_client->setClientSecret('lJFU8tuxzgxyVuI9lRUPJxAA');
+$google_client->setRedirectUri('https://endexam.herokuapp.com/index.php');
 $google_client->addScope('email');
 $google_client->addScope('profile');
 
