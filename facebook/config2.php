522956dead2355299ea6012653ae1c0cce9f8d94
AngeloMartinBHilado <56312511+AngeloMartinBHilado@users.noreply.github.com>
2020-03-20 14:21:42 +0800

    Update config2.php

--- facebook/config2.php
+++ facebook/config2.php
@@ -10,8 +10,8 @@ if (!session_id())
 // Call Facebook API 
 
 $facebook = new \Facebook\Facebook([
-  'app_id'      => '568286157120937',
-  'app_secret'     => '1e3a40ac75b79a33fbf48f70731d4616',
+  'app_id'      => '503929953628834',
+  'app_secret'     => '8ad42e889b06aacd32ecbe58a1955f98',
   'default_graph_version'  => 'v2.10'
 ]);
 
