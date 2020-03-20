bab52757ae218493c16483ecc3a5e81ccb47c53e
AngeloMartinBHilado <56312511+AngeloMartinBHilado@users.noreply.github.com>
2020-03-20 14:19:16 +0800

    Update config.php

--- config.php
+++ config.php
@@ -9,13 +9,13 @@ require_once 'vendor/autoload.php';
 $google_client = new Google_Client();
 
 //Set the OAuth 2.0 Client ID
-$google_client->setClientId('539246690236-tvoad6op2k24t4010o2re4nd9v2kjq1n.apps.googleusercontent.com');
+$google_client->setClientId('889421152360-r47q290i5v29ilfs62cdrfrcth51ac4t.apps.googleusercontent.com');
 
 //Set the OAuth 2.0 Client Secret key
-$google_client->setClientSecret('WpZooC_GLJZXb9zunI3SH828');
+$google_client->setClientSecret('lJFU8tuxzgxyVuI9lRUPJxAA');
 
 //Set the OAuth 2.0 Redirect URI
-$google_client->setRedirectUri('https://maja-etact.herokuapp.com/index.php');
+$google_client->setRedirectUri('https://endexam.herokuapp.com/index.php');
 
 //
 $google_client->addScope('email');
